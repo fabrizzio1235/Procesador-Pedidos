a7ab0b3e997b403b048bad3bf4be1044ec291f86
Pedro Tenorio <A21204150@alumnos.uady.mx>
2026-09-14 01:04:01 -0600

    .

--- src/modelos/Producto.java
+++ src/modelos/Producto.java
@@ -18,20 +18,20 @@ public class Producto {
         this.existencia = existencia;
     }
 
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
     public String getNombre() {
         return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
     }
 
     public int getCantidadSolicitada() {return cantidadSolicitada;}
